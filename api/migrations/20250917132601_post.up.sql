9cc6b585b063247d7294c2cef156a150d66d880c
Lukasz Dabrowski <dabrowskilucas2@gmail.com>
2025-09-21 22:47:50 +0200

    sqlx prepare + migrated posts

--- api/migrations/20250917132601_post.up.sql
+++ api/migrations/20250917132601_post.up.sql
@@ -1,6 +1,7 @@
 create table posts (
-    id uuid primary key not null,
+    id uuid primary key default gen_random_uuid(),
     title text not null,
     owner_id uuid not null references users(id) on delete cascade,
     description text not null,
+    created timestamptz not null default now()
 );
